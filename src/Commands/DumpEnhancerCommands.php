c3b82ab6fda4242f2d1f3bdc9b8962687d95f024
Axel Rutz <axel.rutz@machbarmacher.net>
2018-05-01 22:03:16 +0200

    Untested snapshot

--- src/Commands/DumpEnhancerCommands.php
+++ src/Commands/DumpEnhancerCommands.php
@@ -9,20 +9,22 @@ use Symfony\Component\Console\Input\InputInterface;
 class DumpEnhancerCommands extends DrushCommands {
 
   /**
-   * Options for the sql-dump command.
+   * Options for the sql-dump  and sql-sync commands.
    *
    * @hook option @optionset_table_selection
    * @option gdpr Make a gdpr compliant db dump or sync by obfuscating data.
+   * @default gdpr false
+   *
+   * @todo Can we move this to ::initializeDump()?
    */
-  public function optionsDump($options = ['gdpr' => false]) {
+  public function optionsDump() {
   }
 
   /**
-   * Init the sql-dump command.
+   * Init the sql-dump and sql-sync commands.
    *
-   * @todo Also sql-sync, when it recognizes --extra-dump.
-   *   https://github.com/drush-ops/drush/issues/3512
    * @hook init sql-dump
+   * @hook init sql-sync
    *
    * @throws \Exception
    */
@@ -31,18 +33,23 @@ class DumpEnhancerCommands extends DrushCommands {
       // Bootstrap
       $success = drush_bootstrap_to_phase(DRUSH_BOOTSTRAP_DRUPAL_FULL);
       if (!$success) {
-        throw new \Exception('Can not bootstrap drupal, but --gdpr needs this.');
+        throw new \Exception('Option --gdpr needs an installed drupal site.');
       }
       // Ask API.
       /** @var \Drupal\gdpr_dump\GdprDumpExpressionCollectorInterface $dumpExpressionCollector */
       $dumpExpressionCollector = \Drupal::service('gdpr.dump-expression-collector');
+      if (!$dumpExpressionCollector) {
+        throw new \Exception('Option --gdpr needs enabled gdpr_dump module.');
+      }
       $expressions = $dumpExpressionCollector->getExpressions();
-      $extraDumpOption = escapeshellarg(json_encode($expressions));
+      $extraDumpOption = 'gdpr-expressions=' . escapeshellarg(json_encode($expressions));
       // Append to prior extraDumpOption-dump option.
       if ($input->getOption('extra-dump')) {
         $extraDumpOption = $input->getOption('extra-dump') . ' ' . $extraDumpOption;
       }
       $input->setOption('extra-dump', $extraDumpOption);
+      // @todo Make sure we use the right mysqldump.
+      // Either via $PATH, or via (yet to implement) drush option
     }
   }
 
